a680e6b09793e1d17167a2b1641f69456e658598
Matthius Mlengah <91685851+MatthiusM@users.noreply.github.com>
2024-02-23 11:39:51 +0000

    add variable for animation states

---
+++ Heist-of-Reckoning/Assets/Scripts/Animations/Player/PlayerAnimations.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlayerAnimations 
+{
+    public static class GroundedStates
+    {
+        public static readonly float Idle = 0f;
+        public static readonly float Walk = 0.5f;
+        public static readonly float Run = 1f;
+    }
+}
