6184b7ce4e77e59fc0310e11232bf2ca938a9d86
Pau Haw <91692658+Paul-Haw@users.noreply.github.com>
2024-03-01 00:07:02 +0000

    More changes to Level 1 + Main menu start
    Added BG for main menu
    
    Collisions r on the car but they buggy rn

---
+++ Heist-of-Reckoning/Assets/Scripts/Win Screen/Win Trigger.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class WinTrigger : MonoBehaviour
+{
+    public GameObject WinScreen;
+    public static bool Won = false;
+    void Start()
+    {
+        if (WinScreen != null)
+        {
+            WinScreen.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        print("SOMETHING COLLIDED");
+        if (other.CompareTag("Player")) 
+        {
+            print("IT WAS A PLAYER");
+            ShowUI();
+        }
+    }
+
+    void ShowUI()
+    {
+        if (WinScreen != null)
+        {
+            WinScreen.gameObject.SetActive(true);
+            Won = true;
+
+        }
+    }
+
+   
+}
