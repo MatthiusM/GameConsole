2e36405c4e74b975c760b3e58403a5e564b0c4e8
Matthius Mlengah <91685851+MatthiusM@users.noreply.github.com>
2024-04-20 18:58:11 +0100

    use shoot animation for police

--- Heist-of-Reckoning/Assets/Scripts/FSM/Player/PlayerJumpState.cs
+++ Heist-of-Reckoning/Assets/Scripts/FSM/Player/PlayerJumpState.cs
@@ -23,7 +23,7 @@ namespace FintieStateMachine
 
         private void Jump()
         {
-            stateMachine.CharacterController.Move(Vector3.up * jumpForce * Time.deltaTime);
+            stateMachine.CharacterController.Move(jumpForce * Time.deltaTime * Vector3.up);
 
         }
 
@@ -38,7 +38,7 @@ namespace FintieStateMachine
         }
         public override void Exit()
         {
-            stateMachine.Animator.CrossFadeInFixedTime(stateMachine.PlayerAnimatorHashes.GetHash(PlayerStates.Grounded), 0.1f);
+            
         }
     }
 }
